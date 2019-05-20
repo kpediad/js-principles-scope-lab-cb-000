c4943fd12be9f00a2b841ffdc48d909eaa217e72
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-20 08:27:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName;
+
+function upperCaseCustomerName() {
+  return customerName.toUppercase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'Bob';
+}
+
+const leastFavoriteCustomer = 'Dick';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'not Dick';
+}
